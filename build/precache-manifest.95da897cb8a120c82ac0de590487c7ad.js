d2cea7c49ecf5231654b62aaaeb9812431d1cd89
frogonhills <shifat.ahmed@northsouth.edugit config --global user.email shifat.ahmed@northsouth.edugit config --global user.email shifat.ahmed@northsouth.edu>
2021-11-13 15:07:21 +0600

    build files

---
+++ build/precache-manifest.95da897cb8a120c82ac0de590487c7ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "448ac70bf204b75a533e",
+    "url": "/static/css/main.a41f7637.chunk.css"
+  },
+  {
+    "revision": "448ac70bf204b75a533e",
+    "url": "/static/js/main.448ac70b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "866dfc9986c8ebb0c34b",
+    "url": "/static/js/2.866dfc99.chunk.js"
+  },
+  {
+    "revision": "12a6659ff5467c71b599841286fb8e7f",
+    "url": "/static/media/logo.12a6659f.svg"
+  },
+  {
+    "revision": "8a4ae43b3c6eba5c1783d2440bf1bb14",
+    "url": "/index.html"
+  }
+];
